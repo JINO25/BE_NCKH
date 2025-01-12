729e66a5f92e5172c629d29248af3339381a9d8b
Jino <abhkim0803@gmail.com>
2025-01-12 21:06:03 +0700

    update time zone in webSocketController.js

--- controllers/webSocketController.js
+++ controllers/webSocketController.js
@@ -2,6 +2,7 @@ const wss = require('../config/webSocket');  // Đảm bảo bạn chỉ import 
 const client = require('../config/mqtt');    // Import MQTT client đã được khởi tạo trong mqtt.js
 const { addData, listenToSensorData, getDataFromSensorData } = require('../models/firebase');
 const { handleWaterVolumeToday } = require('../models/ETO_Calculator');
+const { DateTime } = require('luxon');
 
 let lastTimestamp = 0;
 
@@ -15,14 +16,15 @@ client.on('message', async function (topic, message) {
 
     // const date = new Date(parseInt(time, 10) * 1000).toString();
     const date = new Date(parseInt(time, 10) * 1000);
-    const timestamp = new Date();
+    const timestamp = DateTime.now().setZone('Asia/Ho_Chi_Minh');
 
-    const startOfDay = new Date(timestamp);
-    startOfDay.setHours(5, 0, 0, 0); // 5:00 AM
+    const startOfDay = timestamp.startOf('day').plus({ hours: 5, minutes: 30 }); // 5:00 AM
+    const endOfDay = timestamp.startOf('day').plus({ hours: 16, minutes: 30 }); // 4:30 PM
 
-    const endOfDay = new Date(timestamp);
-    endOfDay.setHours(16, 30, 0, 0); // 4:30 PM
-    console.log('time now: ', timestamp.toTimeString());
+    if (timestamp < startOfDay || timestamp > endOfDay) {
+        console.log('Data is outside the time range (5:30 AM - 4:30 PM). Skipping.');
+        return;
+    }
 
 
     if (date < startOfDay || date > endOfDay) {
@@ -56,15 +58,14 @@ client.on('message', async function (topic, message) {
         return;
     }
 
-    // getDataFromSensorData(sendDataToClient);
 
     lastTimestamp = time;
     console.log(`Dữ liệu mới: ${temp}, ${humidity}, ${humidityInSideHouse}`);
 
     // add data to firebase
-    // await addData(today, millisecond, temp, humidity, humidityInSideHouse);
-    // await handleWaterVolumeToday();
-    // listenToSensorData(sendDataToClient);
+    await addData(today, millisecond, temp, humidity, humidityInSideHouse);
+    await handleWaterVolumeToday();
+    listenToSensorData(sendDataToClient);
 });
 
 //Send data to frontend
